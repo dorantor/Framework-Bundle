252a7485142d2aa382997ca1d04a0db1cfbdb80f
Dracony <draconyster@gmail.com>
2017-01-09 18:52:32 +0100

    new structure

---
+++ assets/bundleTemplate/src/HTTP/Greet.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NS\BUNDLE\HTTP;
+
+use PHPixie\HTTP\Request;
+
+class Greet extends Processor
+{
+    /**
+     * Default action
+     * @param Request $request HTTP request
+     * @return mixed
+     */
+    public function defaultAction($request)
+    {
+        $template = $this->components()->template();
+
+        $container = $template->get('app:greet');
+        $container->message = "Have fun coding!";
+        return $container;
+    }
+}
